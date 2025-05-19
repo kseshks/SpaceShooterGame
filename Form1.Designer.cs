fa24b1513c00ddd0f536246f44f94060a01c9156
kseshks <kseniavoronina1211@gmail.com>
2025-05-19 21:57:43 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SpaceShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.spaceship = new System.Windows.Forms.PictureBox();
+            this.comet1 = new System.Windows.Forms.PictureBox();
+            this.comet2 = new System.Windows.Forms.PictureBox();
+            this.comet3 = new System.Windows.Forms.PictureBox();
+            this.comet4 = new System.Windows.Forms.PictureBox();
+            this.comet5 = new System.Windows.Forms.PictureBox();
+            this.comet6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelLose = new System.Windows.Forms.Label();
+            this.bRestart = new System.Windows.Forms.Button();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spaceship)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1064, 628);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // spaceship
+            // 
+            this.spaceship.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("spaceship.BackgroundImage")));
+            this.spaceship.Image = ((System.Drawing.Image)(resources.GetObject("spaceship.Image")));
+            this.spaceship.Location = new System.Drawing.Point(439, 427);
+            this.spaceship.Margin = new System.Windows.Forms.Padding(4);
+            this.spaceship.Name = "spaceship";
+            this.spaceship.Size = new System.Drawing.Size(174, 179);
+            this.spaceship.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.spaceship.TabIndex = 1;
+            this.spaceship.TabStop = false;
+            // 
+            // comet1
+            // 
+            this.comet1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet1.BackgroundImage")));
+            this.comet1.Image = ((System.Drawing.Image)(resources.GetObject("comet1.Image")));
+            this.comet1.Location = new System.Drawing.Point(67, -130);
+            this.comet1.Margin = new System.Windows.Forms.Padding(4);
+            this.comet1.Name = "comet1";
+            this.comet1.Size = new System.Drawing.Size(57, 57);
+            this.comet1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet1.TabIndex = 2;
+            this.comet1.TabStop = false;
+            // 
+            // comet2
+            // 
+            this.comet2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet2.BackgroundImage")));
+            this.comet2.Image = ((System.Drawing.Image)(resources.GetObject("comet2.Image")));
+            this.comet2.Location = new System.Drawing.Point(151, -98);
+            this.comet2.Margin = new System.Windows.Forms.Padding(4);
+            this.comet2.Name = "comet2";
+            this.comet2.Size = new System.Drawing.Size(51, 57);
+            this.comet2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet2.TabIndex = 3;
+            this.comet2.TabStop = false;
+            // 
+            // comet3
+            // 
+            this.comet3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet3.BackgroundImage")));
+            this.comet3.Image = ((System.Drawing.Image)(resources.GetObject("comet3.Image")));
+            this.comet3.Location = new System.Drawing.Point(325, -148);
+            this.comet3.Margin = new System.Windows.Forms.Padding(4);
+            this.comet3.Name = "comet3";
+            this.comet3.Size = new System.Drawing.Size(51, 57);
+            this.comet3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet3.TabIndex = 4;
+            this.comet3.TabStop = false;
+            // 
+            // comet4
+            // 
+            this.comet4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet4.BackgroundImage")));
+            this.comet4.Image = ((System.Drawing.Image)(resources.GetObject("comet4.Image")));
+            this.comet4.Location = new System.Drawing.Point(516, -500);
+            this.comet4.Margin = new System.Windows.Forms.Padding(4);
+            this.comet4.Name = "comet4";
+            this.comet4.Size = new System.Drawing.Size(51, 57);
+            this.comet4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet4.TabIndex = 5;
+            this.comet4.TabStop = false;
+            // 
+            // comet5
+            // 
+            this.comet5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet5.BackgroundImage")));
+            this.comet5.Image = ((System.Drawing.Image)(resources.GetObject("comet5.Image")));
+            this.comet5.Location = new System.Drawing.Point(620, -246);
+            this.comet5.Margin = new System.Windows.Forms.Padding(4);
+            this.comet5.Name = "comet5";
+            this.comet5.Size = new System.Drawing.Size(51, 57);
+            this.comet5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet5.TabIndex = 6;
+            this.comet5.TabStop = false;
+            // 
+            // comet6
+            // 
+            this.comet6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("comet6.BackgroundImage")));
+            this.comet6.Image = ((System.Drawing.Image)(resources.GetObject("comet6.Image")));
+            this.comet6.Location = new System.Drawing.Point(712, -795);
+            this.comet6.Margin = new System.Windows.Forms.Padding(4);
+            this.comet6.Name = "comet6";
+            this.comet6.Size = new System.Drawing.Size(51, 57);
+            this.comet6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.comet6.TabIndex = 7;
+            this.comet6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelLose
+            // 
+            this.labelLose.AutoSize = true;
+            this.labelLose.BackColor = System.Drawing.Color.Black;
+            this.labelLose.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLose.ForeColor = System.Drawing.Color.Red;
+            this.labelLose.Location = new System.Drawing.Point(381, 87);
+            this.labelLose.Name = "labelLose";
+            this.labelLose.Size = new System.Drawing.Size(274, 55);
+            this.labelLose.TabIndex = 8;
+            this.labelLose.Text = "YOU LOST";
+            // 
+            // bRestart
+            // 
+            this.bRestart.BackColor = System.Drawing.Color.LightGreen;
+            this.bRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bRestart.Location = new System.Drawing.Point(448, 174);
+            this.bRestart.Name = "bRestart";
+            this.bRestart.Size = new System.Drawing.Size(196, 70);
+            this.bRestart.TabIndex = 9;
+            this.bRestart.Text = "RESTART";
+            this.bRestart.UseVisualStyleBackColor = false;
+            this.bRestart.Click += new System.EventHandler(this.bRestart_Click);
+            // 
+            // bullet
+            // 
+            this.bullet.Image = ((System.Drawing.Image)(resources.GetObject("bullet.Image")));
+            this.bullet.Location = new System.Drawing.Point(511, 485);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(16, 47);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 10;
+            this.bullet.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 619);
+            this.Controls.Add(this.spaceship);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.bRestart);
+            this.Controls.Add(this.labelLose);
+            this.Controls.Add(this.comet6);
+            this.Controls.Add(this.comet5);
+            this.Controls.Add(this.comet4);
+            this.Controls.Add(this.comet3);
+            this.Controls.Add(this.comet2);
+            this.Controls.Add(this.comet1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spaceship)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comet6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox spaceship;
+        private System.Windows.Forms.PictureBox comet1;
+        private System.Windows.Forms.PictureBox comet2;
+        private System.Windows.Forms.PictureBox comet3;
+        private System.Windows.Forms.PictureBox comet4;
+        private System.Windows.Forms.PictureBox comet5;
+        private System.Windows.Forms.PictureBox comet6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelLose;
+        private System.Windows.Forms.Button bRestart;
+        private System.Windows.Forms.PictureBox bullet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceShooter
+{
+    public partial class Form1 : Form
+    {
+        private bool lose = false;
+        private bool bulletActive = false;
+        private int bulletSpeed = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+            labelLose.Visible = false;
+            bRestart.Visible = false;
+            bullet.Visible = false; // Скрываем пулю в начале игры
+            KeyPreview = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int speed = 30;
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lose) return;
+
+            int speed = 30;
+            if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && spaceship.Left > -30)
+                spaceship.Left -= speed;
+            else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && spaceship.Right < 800)
+                spaceship.Left += speed;
+            else if (e.KeyCode == Keys.Space && !bulletActive) // Стрельба пробелом
+            {
+                ShootBullet();
+            }
+        }
+
+        private void ShootBullet()
+        {
+            bulletActive = true;
+            bullet.Visible = true;
+            bullet.Top = spaceship.Top - bullet.Height;
+            bullet.Left = spaceship.Left + (spaceship.Width / 2) - (bullet.Width / 2);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int cometspeed = 20;
+
+            // Движение комет
+            comet1.Top += cometspeed;
+            comet2.Top += cometspeed;
+            comet3.Top += cometspeed;
+            comet4.Top += cometspeed;
+            comet5.Top += cometspeed;
+            comet6.Top += cometspeed;
+
+            // Движение пули
+            if (bulletActive)
+            {
+                bullet.Top -= bulletSpeed;
+
+                // Проверка выхода пули за границы экрана
+                if (bullet.Top < 0)
+                {
+                    bulletActive = false;
+                    bullet.Visible = false;
+                }
+
+                // Проверка столкновений пули с кометами
+                CheckBulletCollisions();
+            }
+
+            // Перемещение комет при выходе за границы
+            if (comet1.Top >= 600)
+            {
+                comet1.Top = -50;
+                Random rand = new Random();
+                comet1.Left = rand.Next(10, 100);
+            }
+            if (comet2.Top >= 600)
+            {
+                comet2.Top = -100;
+                Random rand = new Random();
+                comet2.Left = rand.Next(100, 200);
+            }
+            if (comet3.Top >= 600)
+            {
+                comet3.Top = -150;
+                Random rand = new Random();
+                comet3.Left = rand.Next(200, 300);
+            }
+            if (comet4.Top >= 600)
+            {
+                comet4.Top = -200;
+                Random rand = new Random();
+                comet4.Left = rand.Next(300, 400);
+            }
+            if (comet5.Top >= 600)
+            {
+                comet5.Top = -250;
+                Random rand = new Random();
+                comet5.Left = rand.Next(500, 600);
+            }
+            if (comet6.Top >= 600)
+            {
+                comet6.Top = -300;
+                Random rand = new Random();
+                comet6.Left = rand.Next(600, 700);
+            }
+
+            // Проверка столкновений корабля с кометами
+            if (spaceship.Bounds.IntersectsWith(comet1.Bounds) ||
+                spaceship.Bounds.IntersectsWith(comet2.Bounds) ||
+                spaceship.Bounds.IntersectsWith(comet3.Bounds) ||
+                spaceship.Bounds.IntersectsWith(comet4.Bounds) ||
+                spaceship.Bounds.IntersectsWith(comet5.Bounds) ||
+                spaceship.Bounds.IntersectsWith(comet6.Bounds))
+            {
+                timer1.Enabled = false;
+                labelLose.Visible = true;
+                bRestart.Visible = true;
+                lose = true;
+            }
+        }
+
+        private void CheckBulletCollisions()
+        {
+            // Проверка столкновений пули с каждой кометой
+            if (bullet.Bounds.IntersectsWith(comet1.Bounds))
+            {
+                HandleBulletHit(comet1);
+            }
+            else if (bullet.Bounds.IntersectsWith(comet2.Bounds))
+            {
+                HandleBulletHit(comet2);
+            }
+            else if (bullet.Bounds.IntersectsWith(comet3.Bounds))
+            {
+                HandleBulletHit(comet3);
+            }
+            else if (bullet.Bounds.IntersectsWith(comet4.Bounds))
+            {
+                HandleBulletHit(comet4);
+            }
+            else if (bullet.Bounds.IntersectsWith(comet5.Bounds))
+            {
+                HandleBulletHit(comet5);
+            }
+            else if (bullet.Bounds.IntersectsWith(comet6.Bounds))
+            {
+                HandleBulletHit(comet6);
+            }
+        }
+
+        private void HandleBulletHit(PictureBox comet)
+        {
+            // Деактивируем пулю
+            bulletActive = false;
+            bullet.Visible = false;
+
+            // Перемещаем комету вверх экрана
+            Random rand = new Random();
+            comet.Top = -rand.Next(50, 300);
+            comet.Left = rand.Next(10, 700);
+
+            // Здесь можно добавить очки или другие эффекты
+        }
+
+        private void bRestart_Click(object sender, EventArgs e)
+        {
+            lose = false;
+            labelLose.Visible = false;
+            bRestart.Visible = false;
+            bulletActive = false;
+            bullet.Visible = false;
+            timer1.Enabled = true;
+
+            comet1.Top = -100;
+            comet2.Top = -500;
+            comet3.Top = -430;
+            comet4.Top = -230;
+            comet5.Top = -180;
+            comet6.Top = -720;
+        }
+    }
+}
